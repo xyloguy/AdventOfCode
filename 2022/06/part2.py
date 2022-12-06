d3f4e14517957c90cba84f32a97c9096e9c13aa4
Bryan Poulsen <bryan.poulsen@printerlogic.com>
2022-12-06 09:10:19 -0700

    make part2 a single line because why not...

--- 2022/06/part2.py
+++ 2022/06/part2.py
@@ -1,7 +1,1 @@
-string = input()
-
-size = 14
-for i in range(0, len(string)-size):
-	if len(set(string[i:i+size])) == size:
-		print(i + size)
-		exit()
+string = input(); size = 14; l = [(i + size) for i in range(0, len(string)-size) if len(set(string[i:i+size])) == size]; print(l[0])
