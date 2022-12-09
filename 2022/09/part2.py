179e6477b9073a225872d468924b99dfb66c5d30
Bryan Poulsen <bryan.poulsen@printerlogic.com>
2022-12-09 09:26:09 -0700

    updated day 9 part2.py for fun

--- 2022/09/part2.py
+++ 2022/09/part2.py
@@ -1,9 +1,5 @@
 from sys import stdin
 
-p = (0,0)
-points = [p for _ in range(10)]
-grid = [{p} for _ in range(10)]
-
 def update_p2(p1, p2):
 	x1, y1 = p1
 	x2, y2 = p2
@@ -28,7 +24,7 @@ def update_p2(p1, p2):
 	return (x2, y2)
 
 
-def print_grid(grid, points):
+def print_grid(grid, points, line=None, show_start=True, show_tail=False, show_knots=True):
 	min_x = max_x = min_y = max_y = 0
 	for i in range(len(grid)):
 		for x, y in grid[i]:
@@ -40,41 +36,63 @@ def print_grid(grid, points):
 		l = ""
 		for x in range(min_x, max_x + 1):
 			c = None
-			for i in range(len(points)):
-				x1, y1 = points[i]
+			if show_knots:
+				for i in range(len(points)):
+					x1, y1 = points[i]
 
-				if x == x1 and y == y1:
-					c = str(i)
-					break
+					if x == x1 and y == y1:
+						c = str(i)
+						if i == 0:
+							c = "H"
+						break
+			if show_start and (x,y) == (0,0) and c is None:
+				c = "s"
+			if show_tail and (x,y) in grid[-1] and c is None:
+				c = "#"
 			l += c or "."
 		print(l)
+
 	print()
+	if line is not None:
+		print(f"== {line.rstrip()} ==")
 	print()
 
 
-for line in stdin:
-	d, c = line.split()
-	for _ in range(int(c)):
-		hx, hy = points[0]
-		if d == "R":
-			hx += 1
-		if d == "L":
-			hx -= 1
-		if d == "U":
-			hy -= 1
-		if d == "D":
-			hy += 1
+def main():
+	p = (0,0)
+	knots = 10
+	points = [p for _ in range(knots)]
+	grid = [{p} for _ in range(knots)]
 
-		points[0] = (hx,hy)
-		grid[0].add((hx,hy))
+	for line in stdin:
+		d, c = line.split()
+		#print_grid(grid, points, line)
+		for _ in range(int(c)):
+			hx, hy = points[0]
+			if d == "R":
+				hx += 1
+			if d == "L":
+				hx -= 1
+			if d == "U":
+				hy -= 1
+			if d == "D":
+				hy += 1
 
-		for i in range(len(points)-1):
-			p1 = points[i]
-			p2 = points[i+1]
-			p2 = update_p2(p1, p2)
-			points[i+1] = p2
-			grid[i+1].add(p2)
+			points[0] = (hx,hy)
+			grid[0].add((hx,hy))
+
+			for i in range(len(points)-1):
+				p1 = points[i]
+				p2 = points[i+1]
+				p2 = update_p2(p1, p2)
+				points[i+1] = p2
+				grid[i+1].add(p2)
+		
+
 	#print_grid(grid, points)
+	#print_grid(grid, points, show_start=True, show_knots=False, show_tail=True)
 
+	print(len(grid[-1]))
 
-print(len(grid[-1]))
+if __name__ == "__main__":
+	main()
